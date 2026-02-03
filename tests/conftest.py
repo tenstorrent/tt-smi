48b972ff13a6a14dfc631e42b6d2ef923b2ce390
Petra Alexson <palexson@tenstorrent.com>
2026-02-03 16:28:09 -0500

    Add UMD backend to tests
    In conftest.py, modify fixtures so that:
    - There are luwen fixtures and for devices and backend with corresponding UMD
    ones
    - The pci_indices fixture has been changed to be reset_test_config, which also
    returns the use_umd param for use in reset tests
    - Add parametrized fixtures to run tests with both luwen and UMD backends
    
    Modify test_reset.py to pick up changes to the fixtures and make use of the
    use_umd param.
    
    Signed-off-by: Petra Alexson <palexson@tenstorrent.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,30 +3,78 @@
 
 import pytest
 
-from typing import List, Dict
+from typing import List, Dict, Tuple, Union
 
 from pyluwen import pci_scan, PciChip
+from tt_umd import PCIDevice, TopologyDiscovery, TTDevice
 from tt_smi.tt_smi_backend import TTSMIBackend
 from tt_tools_common.utils_common.tools_utils import detect_chips_with_callback
 
-
-@pytest.fixture(scope="session")
-def pci_indices() -> List[int]:
-    """
-    Return a list of PCI indices of Tenstorrent devices.
-    """
-    # TODO: Test using the UMD function that provides pci indices
-    return pci_scan()
+# Luwen fixtures
 
 
 @pytest.fixture(scope="session")
-def devices() -> Dict[int, PciChip]:
+def luwen_reset_test_config() -> Tuple[List[int], bool]:
+    """Return a tuple of a list of PCI indices using Luwen backend, and False, which represents the use_umd param."""
+    return (pci_scan(), False)
+
+
+@pytest.fixture(scope="session")
+def luwen_devices() -> Dict[int, PciChip]:
     """Return a list of Tenstorrent PciChips."""
-    # TODO: Test using the UMD function to detect chips
     return dict(enumerate(detect_chips_with_callback()))
 
 
 @pytest.fixture(scope="session")
-def backend(devices) -> TTSMIBackend:
+def luwen_backend(luwen_devices) -> TTSMIBackend:
     """Return a TTSMIBackend instance created from devices."""
-    return TTSMIBackend(devices)
+    return TTSMIBackend(luwen_devices)
+
+
+# UMD fixtures
+
+
+@pytest.fixture(scope="session")
+def umd_reset_test_config() -> Tuple[List[int], bool]:
+    """Return a tuple of a list of PCI indices using UMD backend, and True, which represents the use_umd param."""
+    return (list(PCIDevice.enumerate_devices_info()), True)
+
+
+@pytest.fixture(scope="session")
+def umd_devices() -> dict[int, TTDevice]:
+    """Return a dict of Tenstorrent TTDevices using UMD backend."""
+    _, devices = TopologyDiscovery.discover()
+    return devices
+
+
+@pytest.fixture(scope="session")
+def umd_backend() -> TTSMIBackend:
+    """Return a TTSMIBackend instance using UMD backend."""
+    cluster_descriptor, devices = TopologyDiscovery.discover()
+    return TTSMIBackend(devices=devices, umd_cluster_descriptor=cluster_descriptor)
+
+
+# Parametrized fixtures to run tests with both backends
+
+
+@pytest.fixture(scope="session", params=["luwen", "umd"])
+def reset_test_config(
+    request, luwen_reset_test_config, umd_reset_test_config
+) -> Tuple[List[int], bool]:
+    if request.param == "luwen":
+        return luwen_reset_test_config
+    return umd_reset_test_config
+
+
+@pytest.fixture(scope="session", params=["luwen", "umd"])
+def devices(request, luwen_devices, umd_devices) -> List[Union[PciChip, TTDevice]]:
+    if request.param == "luwen":
+        return luwen_devices
+    return umd_devices
+
+
+@pytest.fixture(scope="session", params=["luwen", "umd"])
+def backend(request, luwen_backend, umd_backend) -> TTSMIBackend:
+    if request.param == "luwen":
+        return luwen_backend
+    return umd_backend
--- tests/test_reset.py
+++ tests/test_reset.py
@@ -3,72 +3,83 @@
 
 import pytest
 
+from typing import List
+
 from pyluwen import pci_scan
+from tt_umd import PCIDevice
 from tt_smi.tt_smi_backend import pci_board_reset, glx_6u_trays_reset
 
 NUM_RESETS_STRESS_TEST = 10
 
 
+def redetect_devices(use_umd: bool) -> List[int]:
+    if use_umd:
+        return list(PCIDevice.enumerate_devices_info())
+    return pci_scan()
+
+
 @pytest.mark.requires_hardware
 class TestPciDriverReset:
-    def test_pci_reset_all_devices(self, pci_indices):
+    def test_pci_reset_all_devices(self, reset_test_config):
         """
         Test resetting all PCI devices (invoked by tt-smi -r).
 
         Passes if the reset is successful and the same number of devices are
         detected before and after.
         """
-        pci_board_reset(pci_indices, reinit=True)
-        post_reset_devices = pci_scan()
+        pci_indices, use_umd = reset_test_config
+        pci_board_reset(pci_indices, reinit=True, use_umd=use_umd)
+        post_reset_devices = redetect_devices(use_umd)
         assert len(post_reset_devices) == len(pci_indices)
 
-
-    def test_pci_reset_all_devices_stress(self, pci_indices):
+    def test_pci_reset_all_devices_stress(self, reset_test_config):
         """
         Test resetting all PCI devices NUM_RESETS_STRESS_TEST times in a row.
 
         Passes if the resets are successful and the same number of devices are
         detected before and after each reset.
         """
+        pci_indices, use_umd = reset_test_config
         for _ in range(NUM_RESETS_STRESS_TEST):
-            pci_board_reset(pci_indices, reinit=True)
-            post_reset_devices = pci_scan()
+            pci_board_reset(pci_indices, reinit=True, use_umd=use_umd)
+            post_reset_devices = redetect_devices(use_umd)
             assert len(post_reset_devices) == len(pci_indices)
 
 
 @pytest.mark.requires_hardware
 @pytest.mark.requires_galaxy
 class TestGalaxyReset:
-    def test_glx_reset(pci_indices):
+    def test_glx_reset(self, reset_test_config):
         """
         Test galaxy 6U trays reset (invoked by tt-smi -glx_reset).
 
         Passes if the reset is successful and the same number of devices are
         detected before and after.
         """
+        pci_indices, use_umd = reset_test_config
         # Expect SystemExit with return code 0 on successful reset
         with pytest.raises(SystemExit) as exc_info:
-            glx_6u_trays_reset()
+            glx_6u_trays_reset(use_umd=use_umd)
 
         assert exc_info.value.code == 0
 
-        post_reset_devices = pci_scan()
+        post_reset_devices = redetect_devices(use_umd)
         assert len(post_reset_devices) == len(pci_indices)
 
-
-    def test_glx_reset_stress(pci_indices):
+    def test_glx_reset_stress(self, reset_test_config):
         """
         Test galaxy 6U trays reset NUM_RESETS_STRESS_TEST times in a row.
 
         Passes if the resets are successful and the same number of devices are
         detected before and after each reset.
         """
+        pci_indices, use_umd = reset_test_config
         for _ in range(NUM_RESETS_STRESS_TEST):
             # Expect SystemExit with return code 0 on successful reset
             with pytest.raises(SystemExit) as exc_info:
-                glx_6u_trays_reset()
+                glx_6u_trays_reset(use_umd=use_umd)
 
             assert exc_info.value.code == 0
 
-            post_reset_devices = pci_scan()
+            post_reset_devices = redetect_devices(use_umd)
             assert len(post_reset_devices) == len(pci_indices)
